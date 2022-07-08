db085104ef8cceffa5567384bf0c96144ca82c70
dhk767 <dhk767@gmail.com>
2022-07-08 14:08:34 +0900

    tdd 추가

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,19 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+
+    @Test
+    public void 실험1() {
+
+        int rs = 10;
+
+        assertEquals(10, rs);
+
+    }
+}
+

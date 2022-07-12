38c154bdff9505cbd1e68e35eedf025a16ffb1cd
dhk767 <dhk767@gmail.com>
2022-07-12 15:54:32 +0900

    7/12 수업 반영

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,5 +1,7 @@
 package com.ll.exam;
 
+import com.ll.Rq;
+import com.ll.Util;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -8,18 +10,49 @@ import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
-
-
     @Test
-    public void 실험1() {
-        int rs = 10;
-        assertEquals(10, rs);
+    void 파일에_내용쓰기() {
+        Util.mkdir("test_data");
+        Util.saveToFile("test_data/1.json", "내용");
     }
 
     @Test
-    // 키보드 입력을 자동으로 하는 테스트
-    public void 문자열을_스캐너입력으로_설정(){
+    public void Rq__getPath() {
+        Rq rq = new Rq("삭제?id=1");
 
+        String path = rq.getPath();
+
+        assertEquals("삭제", path);
+    }
+
+    @Test
+    public void Rq__getIntParam() {
+        Rq rq = new Rq("삭제?id=1");
+
+        int id = rq.getIntParam("id", 0);
+
+        assertEquals(1, id);
+    }
+
+    @Test
+    public void Rq__getIntParam__2() {
+        Rq rq = new Rq("검색?id=10&no=1");
+
+        int id = rq.getIntParam("id", 0);
+        int no = rq.getIntParam("no", 0);
+
+        assertEquals(10, id);
+        assertEquals(1, no);
+    }
+
+    @Test
+    public void 테스트_실험() {
+        int rs = 10 + 20;
+        assertEquals(30, rs);
+    }
+
+    @Test
+    public void 문자열을_스캐너의_입력으로_설정() {
         String input = """
                 등록
                 명언1
@@ -29,11 +62,11 @@ public class AppTest {
         Scanner sc = new Scanner(in);
 
         String cmd = sc.nextLine().trim();
-        String maxim = sc.nextLine().trim();
+        String content = sc.nextLine().trim();
         String author = sc.nextLine().trim();
 
         assertEquals("등록", cmd);
-        assertEquals("명언1", maxim);
+        assertEquals("명언1", content);
         assertEquals("작가1", author);
     }
 
@@ -45,6 +78,7 @@ public class AppTest {
 
         System.out.println("안녕");
 
+        // 그 동안 System.out.println 으로 모아놨던 문장들을 받아옴
         String rs = output.toString().trim();
 
         // 표준출력을 원상복구
@@ -53,4 +87,4 @@ public class AppTest {
 
         assertEquals("안녕", rs);
     }
-}//end class
+}

48c5923759385a0b19897b91075fdfe350f5877c
dhk767 <dhk767@gmail.com>
2022-07-14 16:11:32 +0900

    7/14 수업 내용 반영

--- src/main/java/com/ll/WiseSaying.java
+++ src/main/java/com/ll/WiseSaying.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.Map;
+
 public class WiseSaying {
     int id;
     String maxim;
@@ -10,6 +12,12 @@ public class WiseSaying {
         this.maxim = maxim;
         this.author = author;
     }
+    public WiseSaying(Map<String, Object> map) {
+        this.id = (int) map.get("id");
+        this.maxim = (String) map.get("content");
+        this.author = (String) map.get("author");
+    }
+
 
     @Override
     public String toString() {
@@ -18,5 +26,40 @@ public class WiseSaying {
                 ", maxim='" + maxim + '\'' +
                 ", author='" + author + '\'' +
                 '}';
+    }
+
+    public String toJson() {
+        return """
+                 {
+                     "id": %d,
+                     "content": "%s",
+                     "author": "%s"
+                 }
+                 """
+                .stripIndent()
+                .formatted(id, maxim, author)
+                .trim();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o instanceof WiseSaying == false) return false;
+
+        WiseSaying other = (WiseSaying) o;
+
+        if (this.id != other.id) return false;
+        if (this.maxim.equals(other.maxim) == false) return false;
+        if (this.author.equals(other.author) == false) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (maxim != null ? maxim.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        return result;
     }
 }
